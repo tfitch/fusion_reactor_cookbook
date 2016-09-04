0e1316e2d8e9b0c1a899da719af2268d6dde72a5
Tyler Fitch <tfitch@tbook-air.local>
2016-09-03 18:48:00 -0700

    Set my metadata and LICENSE

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,15 @@
 name 'fusion_reactor'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures fusion_reactor'
-long_description 'Installs/Configures fusion_reactor'
+maintainer 'Tyler Fitch'
+maintainer_email 'git@tfitch.com'
+license 'MIT'
+description 'Installs/Configures Fusion Reactor'
+long_description 'Installs/Configures Fusion Reactor, a Java Server Monitor'
 version '0.1.0'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/fusion_reactor/issues' if respond_to?(:issues_url)
+# issues_url 'https://github.com/tfitch/fusion_reactor_cookbook/issues' if respond_to?(:issues_url)
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/fusion_reactor' if respond_to?(:source_url)
+# source_url 'https://github.com/tfitch/fusion_reactor_cookbook' if respond_to?(:source_url)
